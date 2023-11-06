677c97d798f268e0d1f257d94ee9508e5848971c
ValentineOO <varlomxg@gmail.com>
2023-11-06 14:53:49 +0100

    adding contacts

---
+++ client/src/components/Contact/Contact.jsx
@@ -0,0 +1,40 @@
+import React from "react";
+import { MdCall } from "react-icons/md";
+import { BsFillChatDotsFill } from "react-icons/bs";
+import { HiChatBubbleBottomCenter } from "react-icons/hi";
+
+const Contact = () => {
+  return (
+    <section className="c-wrapper">
+      <div className="paddings innerWidth flexCenter c-container">
+        <div className="flexColStart c-left">
+          <span className="orangeText">Our Contacts</span>
+          <span className="primaryText">Easy to Contact us</span>
+          <span className="secondaryText">
+            {" "}
+            We always ready to help by providing the best services for you. We
+            believe a good place to live can make your life better{" "}
+          </span>
+          <div className="flexColStart contactModes">
+            <div className="flexColStart row">
+              <div className="flexColCenter mode">
+                <div className="flexStart">
+                  <div className="flexCenter icon">
+                    <MdCall size={25} />
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+        <div className="c-right">
+          <div className="image-container">
+            <img src="./contact.jpg" alt="" />
+          </div>
+        </div>
+      </div>
+    </section>
+  );
+};
+
+export default Contact;
